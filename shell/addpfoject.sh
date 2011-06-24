749d7e1150325cb447be540adf9738d225f7fbbc
a.shigeru <a.shigeru@gmail.com>
2011-06-24 12:33:15 +0900

    add script
    svn プロジェクト作成スクリプト

---
+++ shell/addpfoject.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 
+# svn add project
+# 
+
+project=""
+
+usage="Usage: $0 -n PROJECTNAME"
+
+while test -n "$1";
+	do
+		case $1 in
+			-n)
+				project="$2"
+					shift
+					shift
+					continue;;
+
+			--help)
+				echo "$usage";
+				exit 0;;
+		esac
+done
+
+if [ -z $project ]; then
+	echo "$usage"
+	exit 0;
+fi
+
+mkdir $project \
+&& svnadmin create $project \
+&& chmod g+s $project \
+&& chmod 770 $project \
+&& chown -R svn:svn $project
+
+exit 0
