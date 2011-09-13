1001a5cb5fe2542f8b6a3ce43f874934b172b53b
a.shigeru <a.shigeru@gmail.com>
2011-09-13 10:35:16 +0900

    refactor

--- php/translate.php
+++ php/translate.php
@@ -1,10 +1,16 @@
 <?php
+/**
+ * 配列の転地行列
+ * @param array $array
+ */
 function transverse_matrix($array){
 	return call_user_func_array('array_map',array_merge(array(null),$array));
 }
-print_r(transverse_matrix(array(
-    array('Id','Name','Age'),
-    array('1','Taro','15'),
-    array('2','Hanako','16'),
-    array('3','Tom','5400')
-)));
+
+$array = array(
+	array('Id','Name','Age'),
+	array('1','Taro','15'),
+	array('2','Hanako','16'),
+	array('3','Tom','5400'),
+);
+print_r(transverse_matrix($array));
