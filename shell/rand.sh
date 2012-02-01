be313ac36b7177c3ef6ccd6622c27b34820e8bac
a.shigeru <a.shigeru@gmail.com>
2012-02-01 09:49:22 +0900

    ランダム文字生成

---
+++ shell/rand.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+rndstr=""
+while [ `echo ${rndstr} | wc -m` -le 8 ]; do
+  rndnum=`od -N1 -D /dev/urandom | sed "s/0* *//;q"`
+  rnd=`expr ${rndnum} % 75 + 48`
+  if [ ${rnd} -ge 58 -a ${rnd} -le 64 -o ${rnd} -ge 91 -a ${rnd} -le 96 ]; then
+    continue
+  fi
+  rndx=`printf %o ${rnd}`
+  rndstr=${rndstr}`printf \\\"${rndx}"`
+done
+echo ${rndstr}
+
