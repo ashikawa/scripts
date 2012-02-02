0bda216234983aed31ba3bbb72b2656ece9f78c0
a.shigeru <a.shigeru@gmail.com>
2012-02-02 12:09:11 +0900

    rand.sh 機能追加

--- shell/rand.sh
+++ shell/rand.sh
@@ -2,12 +2,13 @@
 rndstr=""
 while [ `echo ${rndstr} | wc -m` -le 8 ]; do
   rndnum=`od -N1 -D /dev/urandom | sed "s/0* *//;q"`
-  rnd=`expr ${rndnum} % 75 + 48`
+  rnd=`expr ${rndnum} % 75 + 48` 
   if [ ${rnd} -ge 58 -a ${rnd} -le 64 -o ${rnd} -ge 91 -a ${rnd} -le 96 ]; then
     continue
-  fi
+  fi  
   rndx=`printf %o ${rnd}`
   rndstr=${rndstr}`printf \\\"${rndx}"`
 done
 echo ${rndstr}
-
+# ついでにクリップボードにコピー
+echo ${rndstr} | pbcopy
