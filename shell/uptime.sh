b35dcea42a8c4db5992db0c03b67b3fc12dbc558
a.shigeru <a.shigeru@gmail.com>
2013-09-12 15:09:05 +0900

    uptime.sh

--- shell/uptime.sh
+++ shell/uptime.sh
@@ -4,13 +4,13 @@
 # check load average and echo server status
 #
 
-LOAD=`uptime | awk '{print $8}' | sed -e "s/,//g"`
+LOAD=`uptime | awk '{print $9}' | sed -e "s/,//g"`
 FLAG=`echo "$LOAD > 5.0" | bc`
 
-if [ $FLAG -eq 1 ]; then
+if [ "$FLAG" -eq 1 ]; then
 	echo "== top n 1 b =="
 	top n 1 b
-	echo "== ps -ux =="
+	echo "== ps aux =="
 	ps aux
 	echo "== netstat -an =="
 	netstat -an
