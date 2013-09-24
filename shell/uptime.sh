804111b44cd45c3f3ded1dacd3c13517bf605311
a.shigeru <a.shigeru@gmail.com>
2013-09-24 11:43:47 +0900

    update

--- shell/uptime.sh
+++ shell/uptime.sh
@@ -3,19 +3,22 @@
 #
 # check load average and echo server status
 #
+# cron
+# */10 * * * * /path/to/watch.sh
 
-LOAD=`uptime | awk '{print $9}' | sed -e "s/,//g"`
-FLAG=`echo "$LOAD > 5.0" | bc`
+LOAD=`uptime | awk '{print $10}' | sed -e "s/,//g"`
+FLAG=`echo "$LOAD >= 1.0" | bc`
 
 if [ "$FLAG" -eq 1 ]; then
-	echo "== top n 1 b =="
-	top n 1 b
-	echo "== ps aux =="
-	ps aux
-	echo "== netstat -an =="
-	netstat -an
-	echo "== df -i =="
-	df -i
-#else
- # Nothing to do
+{
+    echo "=====" `date`  "====="
+    echo "=== uptime ==="
+    uptime
+    echo "=== free ==="
+    free
+    echo "=== vmstat ==="
+    vmstat
+    echo "=== ps aux ==="
+    ps aux
+} | /usr/sbin/rotatelogs /home/ec2-user/watch/watch_log 86400 -f3
 fi
