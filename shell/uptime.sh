c54463b736c6c01781a0bfe00472acd32f668550
s.ashikawa <a.shigeru@gmail.com>
2013-12-02 12:33:29 +0900

    Update uptime.sh

--- shell/uptime.sh
+++ shell/uptime.sh
@@ -6,6 +6,9 @@
 # cron
 # */10 * * * * /path/to/watch.sh
 
+echo "=====" `date`  "=====" >> /home/ec2-user/watch/mem.`date +"%Y%m%d"`
+ps alx  | awk '{printf ("%d\t%s\n", $8,$13)}' | sort -nr | head -20 >> /home/ec2-user/watch/mem.`date +"%Y%m%d"`
+
 LOAD=`uptime | awk '{print $10}' | sed -e "s/,//g"`
 FLAG=`echo "$LOAD >= 1.0" | bc`
 
